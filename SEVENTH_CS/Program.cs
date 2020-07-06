2b40817ec7c50959b7faa167a893f408b24b3453
ChildOfJustice <51397216+ChildOfJustice@users.noreply.github.com>
2020-07-06 18:17:57 +0300

    I hope it will be the last fix

--- SEVENTH_CS/Program.cs
+++ SEVENTH_CS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SEVENTH_CS
@@ -12,21 +13,184 @@ namespace SEVENTH_CS
         static void Main(string[] args)
         {
             
-            Console.WriteLine(args[1]);
+            //Console.WriteLine(args[0]);
+            //Console.WriteLine(args[1]);
             
+            var destinationDirName = args[2];
 
-            try
+            var dirInfoForQuestions = new DirectoryInfo(args[0]);
+            var dirInfoForGroups = new DirectoryInfo(args[1]);
+            var destinationDir = new DirectoryInfo(destinationDirName);
+
+            if (!dirInfoForQuestions.Exists)
             {
-                var dirInfoForQuestions = new DirectoryInfo(args[1]);
-                var dirInfoForGroups = new DirectoryInfo(args[2]);
-
-
-
-
-            } catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                throw new FileNotFoundException($"There is no such directory: {args[0]}");
             }
+            if (!dirInfoForGroups.Exists)
+            {
+                throw new FileNotFoundException($"There is no such directory: {args[1]}");
+            }
+            if (!destinationDir.Exists)
+            {
+                throw new FileNotFoundException($"There is no such directory: {args[2]}");
+            }
+
+            var questionFiles = MakeQuestionFilesArray(dirInfoForQuestions);
+            var availableQuestions = new LinkedList<FileInfo>(questionFiles);
+            Console.WriteLine($"Available Questions: {availableQuestions.Count}");
+
+            var groupsFiles = MakeGroupFilesArray(dirInfoForGroups);
+
+                
+            foreach (var groupFile in groupsFiles)
+            {
+                //FileStream sourceStream = File.Open(groupFileName, FileMode.Open);
+                int counterForIdenticalFIOs = 0;
+                using (StreamReader sr = new StreamReader(File.Open(groupFile.FullName, FileMode.Open)))
+                {
+                    var allFIOs = new List<List<string>>();
+                    string firstLine;
+                    Console.WriteLine($"All students from {groupFile.Name}:");
+                    while ((firstLine = sr.ReadLine()) != null)
+                    {
+                        allFIOs.Add(CreateFIO(firstLine));
+                        Console.WriteLine($"    {firstLine}");
+                    }
+                    Console.WriteLine();
+                    foreach (var currentFIO in allFIOs)
+                    {
+                        //Get a random question:
+                        Random rnd = new Random(new DateTime().Millisecond);
+                        int rand = rnd.Next() % availableQuestions.Count;
+                        var item = availableQuestions.First;
+                        for (int i = 0; i < rand; i++)
+                        {
+                            item = item.Next;
+                        }
+                        var randQuestionFile = item.Value;
+                        availableQuestions.Remove(item);
+                        //^
+
+
+                        //creating appropriate result file name 
+                        int identicalCounter = 0;
+                        var appropriateResultFileName = new StringBuilder();
+                        appropriateResultFileName.Append(groupFile.Name);
+                        appropriateResultFileName.Append("_");
+                        var fullyIdentical = false;
+                        var partiallyIdentical = false;
+
+                        List<string> theMostIdenticalFIO = null;
+                        int maxIdenticalLetters = 0;
+                        foreach (var fio in allFIOs)
+                        {
+
+                            if (currentFIO[0] == fio[0] && currentFIO[1] == fio[1] && currentFIO[2] == fio[2])
+                            {
+                                //fully
+                                identicalCounter++;
+                                if(identicalCounter != 2)
+                                {
+                                    continue;
+                                }
+                                Console.WriteLine("FULLY IDENTICAL <----------(!)");
+                                fullyIdentical = true;
+                                counterForIdenticalFIOs++;
+                                appropriateResultFileName.Append(currentFIO[0]);
+                                appropriateResultFileName.Append("_");
+                                appropriateResultFileName.Append(currentFIO[1]);
+                                appropriateResultFileName.Append(" ");
+                                appropriateResultFileName.Append(currentFIO[2]);
+                                appropriateResultFileName.Append("_");
+                                appropriateResultFileName.Append(counterForIdenticalFIOs);
+                                break;
+                            }
+                            if (currentFIO[0] == fio[0] && currentFIO[1][0] == fio[1][0] && currentFIO[2][0] == fio[2][0])
+                            {
+                                //partially
+                                partiallyIdentical = true;
+                                int identicalLetters = 0;
+
+                                ///var nameAndPatronymic = 
+
+                                for (int i = 0; i < Math.Max(currentFIO[1].Length, fio[1].Length); i++)
+                                {
+                                    if (i == currentFIO[1].Length || i == fio[1].Length || currentFIO[1][i] != fio[1][i])
+                                    {
+                                        if (identicalLetters > maxIdenticalLetters)
+                                        {
+                                            theMostIdenticalFIO = fio;
+                                        }
+                                        break;
+                                    }
+                                    
+                                    identicalLetters++;
+                                }
+                                for (int i = 0; i < Math.Max(currentFIO[2].Length, fio[2].Length); i++)
+                                {
+                                    if (i == currentFIO[2].Length || i == fio[2].Length || currentFIO[2][i] != fio[2][i])
+                                    {
+                                        if (identicalLetters > maxIdenticalLetters)
+                                        {
+                                            theMostIdenticalFIO = fio;
+                                        }
+                                        break;
+                                    }
+                                    
+                                    identicalLetters++;
+                                    
+                                }
+                            }
+                        }
+                        if (!partiallyIdentical && !fullyIdentical)
+                        {
+                            appropriateResultFileName.Append(currentFIO[0]);
+                            appropriateResultFileName.Append("_");
+                            appropriateResultFileName.Append(currentFIO[1][0]);
+                            appropriateResultFileName.Append(" ");
+                            appropriateResultFileName.Append(currentFIO[2][0]);
+                        }
+                        else
+                        if (partiallyIdentical && !fullyIdentical)
+                        {
+                            if (theMostIdenticalFIO == null)
+                            {
+                                throw new Exception("very strange error");
+                            }
+                            appropriateResultFileName.Append(currentFIO[0]);
+                            appropriateResultFileName.Append("_");
+                            for (int i = 0; i < currentFIO[1].Length; i++)
+                            {
+                                if (i == currentFIO[1].Length || i == theMostIdenticalFIO[1].Length || currentFIO[1][i] != theMostIdenticalFIO[1][i])
+                                {
+                                    appropriateResultFileName.Append(currentFIO[1][i]);
+                                    appropriateResultFileName.Append(" ");
+                                    break;
+                                }
+                                appropriateResultFileName.Append(currentFIO[1][i]);
+                            }
+                            for (int i = 0; i < currentFIO[2].Length; i++)
+                            {
+                                if (i == currentFIO[2].Length || i == theMostIdenticalFIO[2].Length || currentFIO[2][i] != theMostIdenticalFIO[2][i])
+                                {
+                                    appropriateResultFileName.Append(currentFIO[2][i]);
+                                    break;
+                                }
+                                appropriateResultFileName.Append(currentFIO[2][i]);
+
+
+                            }
+                        }
+                            
+                        Console.WriteLine(appropriateResultFileName.ToString());
+
+                        File.Copy(randQuestionFile.FullName, Path.Combine(destinationDirName, appropriateResultFileName.ToString() + ".pdf"));
+                    }
+                }
+            }
+
+
+            
 
 
 
@@ -35,11 +199,70 @@ namespace SEVENTH_CS
 
 
             Console.ReadLine();
+            
+            foreach (var file in destinationDir.GetFiles())
+            {
+                File.Delete(file.FullName);
+            }
+
         }
 
-        private static void PrepareQuestionsForExam()
+        private static List<FileInfo> MakeGroupFilesArray(DirectoryInfo dirInfo)
         {
-            throw new NotImplementedException();
+            var files = new List<FileInfo>();
+            foreach (var file in dirInfo.GetFiles())
+            {
+                Regex regex = new Regex(@"(\w*)\.txt$");
+                MatchCollection matches = regex.Matches(file.Name);
+                if (matches.Count == 1 && file.Name[0] != '0')
+                {
+                    files.Add(file);
+                }
+            }
+            return files;
         }
+        private static List<FileInfo> MakeQuestionFilesArray(DirectoryInfo dirInfo, List<FileInfo> files = null)
+        {
+            if(files == null)
+            {
+                files = new List<FileInfo>();
+            }
+            
+            foreach (var file in dirInfo.GetFiles())
+            {
+                Regex regex = new Regex(@"(\w*)\.pdf$");
+                MatchCollection matches = regex.Matches(file.Name);
+                if (matches.Count == 1)
+                {
+                    files.Add(file);                    
+                }
+            }
+            foreach (var dir in dirInfo.GetDirectories())
+            {
+                files = MakeQuestionFilesArray(dir, files);
+            }
+            return files;
+        }
+        private static List<string> CreateFIO(string line)
+        {
+            var toRet = new List<string>();
+            var sb = new StringBuilder();
+            var buf = new StringBuilder();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == ' ')
+                {
+                    toRet.Add(buf.ToString());
+                    buf.Clear();
+                    continue;
+                }
+                buf.Append(line[i]);
+            }
+            toRet.Add(buf.ToString());
+
+            return toRet;
+        }
+
+
     }
 }
